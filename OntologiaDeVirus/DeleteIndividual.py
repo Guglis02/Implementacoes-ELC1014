a0c392e472173ef81b47c805aa3a36c6400b5735
Gustavo Machado <gustavomf02@gmail.com>
2023-05-20 20:30:50 -0300

    Provavelmente versão final
    A partir de agora vou parar de adicionar informação e começar a botar consultas

--- OntologiaDeVirus/DeleteIndividual.py
+++ OntologiaDeVirus/DeleteIndividual.py
@@ -7,37 +7,6 @@ from unidecode import unidecode
 # Criação/Carregamento da ontologia
 onto = get_ontology("file://virus_host_ontology.owl").load()
 
-def GetVirusData():
-    wb = openpyxl.load_workbook('VMR_MSL38_v1.xlsx')
-    sheet = wb.active
-    virus_data = []
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        virus_name = row[18]
-        scientific_name = row[16]
-        abbreviated_name = row[19]
-        lineage = row[2:15]
-        baltimore_group = row[24]
-        host_category = row[25]
-
-        virus_data.append((virus_name, scientific_name, abbreviated_name, lineage, baltimore_group, host_category))
-    return virus_data
-
-def GetLineage(entry):
-    if "viria" in entry:
-        return (Realm(entry))
-    elif "vira" in entry:
-        return (Subrealm(entry))
-    elif "virae" in entry:
-        return (Kingdom(entry))
-    elif "virites" in entry:
-        return (Subkingdom(entry))
-    elif "viricota" in entry:
-        return (Phylum(entry))
-    elif "viricotina" in entry:
-        return (Subphylum(entry))
-    elif "viricetes" in entry:
-        return (Class(entry))
-
 with onto:
     # Classes
     class Species(Thing):
@@ -148,28 +117,9 @@ with onto:
         range = [Virus]
         inverse_property = onto.HasHost
 
-
-    virus_data = GetVirusData()
-    for virus_name, scientific_name, abbreviated_name, lineage, baltimore_group, host_category in tqdm(virus_data):
-        virus = Virus(unidecode(virus_name.replace(' ', '_')))
-        virus.HasForScientificName.append(scientific_name)
-        virus.HasForAbbreviatedName.append(abbreviated_name)
-
-        for entry in lineage:
-            if entry is None or GetLineage(entry) is None:
-                continue
-            virus.FromLineage.append(GetLineage(entry))
-
-        baltimoreGroup = BaltimoreGroup(baltimore_group)
-        virus.FromBaltimoreGroup.append(baltimoreGroup)
-        baltimoreGroup.BelongsToThisGroup.append(virus)
-
-        if "," in host_category:
-            for host in host_category.split(','):
-                virus.HasHostOfThisCategory.append(HostCategory(unidecode(host.replace(' ', ''))))
-        else:
-            virus.HasHostOfThisCategory.append(HostCategory(unidecode(host_category.replace(' ', ''))))
-            
+    for individual in tqdm(Virus.instances()):
+        if not HasGenomeType in individual:
+            destroy_entity(individual)
     # Salvando a ontologia em um arquivo .owl
 onto.save(file="virus_host_ontology.owl", format="rdfxml")
 
