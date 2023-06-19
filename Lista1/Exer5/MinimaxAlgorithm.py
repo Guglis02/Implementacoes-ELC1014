0d71e94050e73545c5552f8341dff552a0973e23
Gustavo Machado <gustavomf02@gmail.com>
2023-06-18 23:44:56 -0300

    Termina de comentar códigos

--- Lista1/Exer5/MinimaxAlgorithm.py
+++ Lista1/Exer5/MinimaxAlgorithm.py
@@ -1,6 +1,7 @@
 # Dados 10 palitos cada jogador pode retirar 1, 2 ou 3 por turno. 
 # Perde o jogador que retira o último palito.
 
+# Classe que representa um nó da estrutura
 class Node:
     def __init__(self, parent, gameState, IsMaxTurn):
         self.parent = parent
@@ -33,38 +34,39 @@ class Node:
     def __eq__(self, __value: object) -> bool:
         return self.gameState == __value.gameState
  
-def minimax(node, depth, is_max_player, indent=0):
+# Função minimax
+def minimax(node, depth, isMaxTurn, indent=0):
     # Verifica se o jogo acabou ou se chegou na profundidade máxima
     if node.gameState == 0 or depth == 0:
         return node.evaluate()
 
     # Define o valor inicial do melhor valor de acordo com o jogador atual
-    best_value = float('-inf') if is_max_player else float('inf')
+    bestValue = float('-inf') if isMaxTurn else float('inf')
 
     # Imprime o estado atual da árvore de busca com o valor de min e max propagados
-    print(f"{' ' * indent}GameState: {node.gameState}, {'Vez do MAX' if is_max_player else 'Vez do MIN'}")
+    print(f"{' ' * indent}GameState: {node.gameState}, {'Vez do MAX' if isMaxTurn else 'Vez do MIN'}")
 
     # Gera todas as possíveis jogadas
     for child in node.GenerateNewNodes():
         # Chama a função minimax recursivamente para o próximo estado
-        value = minimax(child, depth - 1, not is_max_player, indent + 4)
+        value = minimax(child, depth - 1, not isMaxTurn, indent + 4)
 
         # Atualiza o melhor valor de acordo com o jogador atual
-        if is_max_player:
-            best_value = max(best_value, value)
+        if isMaxTurn:
+            bestValue = max(bestValue, value)
         else:
-            best_value = min(best_value, value)
+            bestValue = min(bestValue, value)
 
-    return best_value
+    return bestValue
 
 # Define o estado inicial do jogo
 initial_state = Node(None, 10, True)
 
 # Chama a função minimax para o estado inicial
-best_value = minimax(initial_state, 10, True)
+bestValue = minimax(initial_state, 10, True)
 
 # Verifica se o jogador MAX ganhou ou perdeu
-if best_value == 1:
+if bestValue == 1:
     print("MAX ganhou!")
 else:
     print("MAX perdeu!")
